474c73888ee7246120ee94bd7e8f9b9774d82f79
EXTER <exter7@gmail.com>
2016-06-02 18:22:13 -0400

    Release 1.3.2.0

--- src/main/java/exter/substratum/ModSubstratum.java
+++ src/main/java/exter/substratum/ModSubstratum.java
@@ -37,7 +37,7 @@ public class ModSubstratum
 {
   public static final String MODID = "substratum";
   public static final String MODNAME = "Substratum";
-  public static final String MODVERSION = "1.3.1.1";
+  public static final String MODVERSION = "1.3.2.0";
 
   @Instance(MODID)
   public static ModSubstratum instance;
